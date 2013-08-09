706fb999f8337171dce395aa1d44beb3c74527e4
rsgoncalves <goncalvjr@gmail.com>
2013-08-09 01:54:31 +0100

    updates

--- src/uk/ac/manchester/cs/diff/utils/ReasonerLoader.java
+++ src/uk/ac/manchester/cs/diff/utils/ReasonerLoader.java
@@ -84,7 +84,8 @@ public class ReasonerLoader {
 	 * @return Reasoner instance
 	 */
 	public OWLReasoner createFactReasoner() {
-		SimpleConfiguration config = new SimpleConfiguration(new ConsoleProgressMonitor());
+		SimpleConfiguration config = new SimpleConfiguration(FreshEntityPolicy.ALLOW, Long.MAX_VALUE);
+//		SimpleConfiguration config = new SimpleConfiguration(new ConsoleProgressMonitor());
 		OWLReasonerFactory fac = new FaCTPlusPlusReasonerFactory();
 		
 		if(verbose) System.out.print("   Creating reasoner... ");
